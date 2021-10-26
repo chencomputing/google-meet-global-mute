36fbb2250f1f25b2bc1bcd315d20a0b0ef4046fb
David Chen <david@chenware.com>
2021-10-25 21:08:43 -0700

    Show mute status and toggle it in the prejoin room

--- content-script.js
+++ content-script.js
@@ -10,7 +10,10 @@ chrome.runtime.onMessage.addListener(
             });
             document.dispatchEvent(keypress);
 
-            sendResponse({status: 'success'});
+            // isMuted returns the opposite from it's actual status when called
+            // from this listener here and I don't know why. Inverting the result
+            // is always successful though so.../shrug
+            sendResponse({status: !isMuted() ? 'muted' : 'unmuted'});
         } else {
             sendResponse({status: 'acknowledged'});
         }
@@ -18,3 +21,53 @@ chrome.runtime.onMessage.addListener(
         return true;
     }
   );
+
+window.addEventListener('load', function() {
+    setupPrejoinRoom();
+});
+
+function setupPrejoinRoom() {
+    if (!isPrejoin()) {
+        return;
+    }
+
+    let muteElement = getMuteElement();
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'attributes') {
+                if (mutation.attributeName === 'data-is-muted') {
+                    console.log('The ' + mutation.attributeName + ' attribute was modified: ' + mutation.target.dataset.isMuted);
+                }
+            }
+        }
+    });
+
+    observer.observe(muteElement, { attributes: true });
+}
+
+function getMuteElement() {
+    return document.querySelectorAll('[data-is-muted][aria-label*="microphone"]')[0];
+}
+
+function isPrejoin() {
+    // data-is-green-room works too
+    return !!document.querySelector('[data-is-prejoin]')
+}
+
+function isPostMeeting() {
+    return !!document.querySelector('[data-is-connection-lost]')
+}
+
+function isMuted() {
+    let muteElement = getMuteElement();
+
+    if (muteElement) {
+        let muted = muteElement.dataset.isMuted === 'true';
+        console.log(`Muted: ${muted}`);
+
+        return muted;
+    }
+
+    return false;
+}
